c326deef8b9ea344feb8231bd1e20fe1cc9fb52c
HYP <119740762+herlambangyp@users.noreply.github.com>
2026-07-28 00:03:48 +0700

    Refactor dataServer object syntax in versi.js

--- versi.js
+++ versi.js
@@ -3,9 +3,9 @@ window.dataServer = {
     perubahan: `
     - Menambahkan kotoba minna sampai bab 50
     - Menambahkan teks tombol untuk melihat apa yang diupdate (dibawah tombol update). Agar user bisa membaca changelog
-    `
+    `,
 
-    3.1.8:`
+    '3.1.8':`
     - Anchor point versi online dibuat 
     `
 };
