411ca09eb2bd2546c523941e40fa946e426489b1
HYP <119740762+herlambangyp@users.noreply.github.com>
2026-07-28 00:00:02 +0700

    Update versi.js

--- versi.js
+++ versi.js
@@ -1,4 +1,11 @@
 window.dataServer = {
-    versi: "3.1.8",
-    changelog: "Update tersedia"
+    versi: "3.1.9",
+    perubahan: `
+    - Menambahkan kotoba minna sampai bab 50
+    - Menambahkan teks tombol untuk melihat apa yang diupdate (dibawah tombol update). Agar user bisa membaca changelog
+    `
+
+    3.1.8:`
+    - Anchor point versi online dibuat 
+    `
 };
