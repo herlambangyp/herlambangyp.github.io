a74a61b4353d72a0d32e6b3bd2f4cc5178ccf58c
HYP <119740762+herlambangyp@users.noreply.github.com>
2026-07-28 11:12:14 +0700

    Add files via upload

--- versi.js
+++ versi.js
@@ -1,12 +1,20 @@
 window.dataServer = {
 
-    versi: "3.1.9",
+    versi: "3.1.10",
     changeLog: `
-    - Menambahkan kotoba minna sampai bab 50
-    - Menambahkan teks tombol untuk melihat apa yang diupdate (dibawah tombol update). Agar user bisa membaca changelog
-    `,
+            - Menambahkan tabel kotoba buku Minna no Nihonggo
+            - Menambahkan teks tombol untuk melihat changelog. Sekarang tidak perlu ada update untuk melihat changelog
+            - Menghapus footnote di area tabel
+            - Menambahkan tombol "back to top" agar memudahkan navigasi scroll ke atas
+            - Perbaikan beberapa bug typo nama judul di beberapa mode    
+            `,
 
     changeLogVersiLama:`
-      3.1.8 :
-      - Anchor point versi online dibuat`
+
+        3.1.9 :
+            - Menambahkan kotoba minna sampai bab 50
+            - Menambahkan teks tombol untuk melihat apa yang diupdate (dibawah tombol update). Agar user bisa membaca changelog
+
+        3.1.8 :
+            - Anchor point versi online dibuat`
 };
