fe040b384c3a5971af267bb474205f26c372c108
HYP <119740762+herlambangyp@users.noreply.github.com>
2026-07-29 01:48:13 +0700

    Add files via upload

--- versi.js
+++ versi.js
@@ -1,15 +1,19 @@
 window.dataServer = {
 
-    versi: "3.1.10",
+    versi: "3.1.10a",
     changeLog: `
+            - Memperbaiki bug ikon panah keatas tidak muncul di mobile
+            - Menambahkan toast sebagai pesan eror, alih-alih alerts web yang kurang stabil 
+            `,
+
+    changeLogVersiLama:`
+
+        3.1.10
             - Menambahkan tabel kotoba buku Minna no Nihonggo
             - Menambahkan teks tombol untuk melihat changelog. Sekarang tidak perlu ada update untuk melihat changelog
             - Menghapus footnote di area tabel
             - Menambahkan tombol "back to top" agar memudahkan navigasi scroll ke atas
             - Perbaikan beberapa bug typo nama judul di beberapa mode    
-            `,
-
-    changeLogVersiLama:`
 
         3.1.9 :
             - Menambahkan kotoba minna sampai bab 50
