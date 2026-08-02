fbb64b6fee0c9d9706c7af72ccb27a4f687e04ae
HYP <119740762+herlambangyp@users.noreply.github.com>
2026-08-02 23:23:46 +0700

    Add files via upload

--- versi.js
+++ versi.js
@@ -1,17 +1,19 @@
 window.dataServer = {
 
-    versi: "3.1.12",
+    versi: "3.1.12a",
     changeLog: `
-            - Menambahkan Ui baru untuk menentukan seperti apa bentuk soal di beberapa mode
-            - Menghapus tombol reverse mode karena fungsinya yang sudah digantikan 
-              Ui baru
-            - Mengubah beberapa warna tombol agar lebih seragam
-            - Memperbaiki bug, jumlah soal yang tidak sesuai (selalu kurang 1, tetapi quiz
-              tiba-tiba sudah selesai)
+            - Memperbaiki bug notifikasi batasan jumlah soal yang muncul di temoat yang salah
 
             `,
 
     changeLogVersiLama:`
+
+        3.1.12 
+            - Menambahkan info berapa jumlah soal yang akan dipakai di mode artikan dan baca kanji
+            - Menambahkan batasan jumlah soal yang dipakai di quiz. 
+              Sekarang user bisa mengatur jumlah soal yang ingin dipakai di quiz, 
+              agar tidak terlalu banyak soal yang harus dijawab
+            - Memperbaiki bug judul mode yang salah saat di mode reversed  
 
         3.1.11 
             - Menambahkan info berapa jumlah soal yang akan dipakai di mode artikan dan baca kanji
