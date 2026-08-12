7f38262d3a66531fec49635e97d46e9b88c69bca
HYP <119740762+herlambangyp@users.noreply.github.com>
2026-08-12 21:31:41 +0700

    menambahkan file datset dan helper
    menambahkan dataset japan dan helper generator link download

---
+++ elOnl.js
@@ -0,0 +1,31 @@
+window.linkDown = {
+
+    linkDownloadDataset:`<div class="kotakSubMenu">
+      <div class="titleSubMenu">
+        <span class="judulSubMenu">Dataset Huruf Jepang</span>
+        <span class="keteranganSubMenu">
+          Dibuat oleh Developer <i>native japanese</i> 
+          @<a style="color:darkgray" href="https://github.com/parsimonhi/animCJK">parsimonhi.</a> 
+          Semua bentuk, stroke order, animasi dll didasarkan dari repositorynya
+          <br><span class="text-kecil">
+          Dataset berisi karakter huruf jepang seperti:
+          <ul>
+            <li>Hiragana <b>(86 huruf)</b></li>
+            <li>Katakana <b>(91 huruf)</b></li>
+            <li>Jōyō kanji | kanji JLPT N5-N1 <b>(2136 huruf)</b></li>
+            <li>Jinmeyō Kanji | kanji nama <b>(863 huruf)</b></li>
+            <li>Hyōgai Kanji | kanji kuno <b>(tidak dijelas ada berapa)</b></li>
+            <li>komponen kanji</li>
+            <li>dan beberapa goresan pembantu</li>
+          </ul>
+          Setidaknya ada <b>7000+</b> huruf dalam dataset ini.
+          </span>
+        </span>
+      </div>
+
+      <div class="kotakTombolPilihanMenu">
+        <button class="tombolPilihanMenu"onclick="modeTerpilih('quizMM', 'Test Soal Matematika IM');">Download Dataset (21Mb)</button>
+      </div>
+      
+    </div>`
+}
